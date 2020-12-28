da8c5553bbbb4572fa70c2794e95f75bf0845ea9
Olga112233 <olga.trubnikova@gmail.com>
2020-12-28 14:50:22 +0200

    homework4new

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
-for(let i = 10; i <= 100; i = i + 10){
-    const multiplicationResult = i * 30;
+const oneUSDInUAH = 30;
+for(let i = 10; i <= 100; i += 10){
+    const multiplicationResult = i * oneUSDInUAH;
     console.log(`${i} $ * 30 = ${multiplicationResult} UAH`);
 }
--- script5.js
+++ script5.js
@@ -1,9 +1,10 @@
-let number = prompt("Enter number");
-console. log("Number", number);
-while (number % 3 === 0) {
-    number /= 3;    
-}
-alert((number === 1)? "Number can be obtained" : "Number can not be obtained")
 
- 
+const number = prompt("Enter number");
+let numberResult=number;
+console. log("Number", number);
+while (numberResult % 3 === 0) {
+    numberResult /= 3;    
+}
+console.log((numberResult === 1)? "Number can be obtained 3 in power "+ Math.round(Math.log(number)) : "Number can not be obtained")
+
 
